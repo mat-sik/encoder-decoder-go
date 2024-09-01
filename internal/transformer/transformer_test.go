d51f5637338775df19616415f883bdae1d1e71b6
mateusz <siiikor.sikorski@gmail.com>
2024-09-01 15:46:37 +0200

    Finished writing tests to function used for transforming runes in buffers.

--- internal/transformer/transformer_test.go
+++ internal/transformer/transformer_test.go
@@ -2,30 +2,127 @@ package transformer
 
 import (
 	"bytes"
-	"fmt"
 	"github.com/mat-sik/encoder-decoder/internal/algorithms"
+	"github.com/stretchr/testify/assert"
 	"testing"
 	"unicode/utf8"
 )
 
-func Test_transformRuneBuffers(t *testing.T) {
-	// given
-	airplane := '\u2708'
-	byteAirplane := make([]byte, utf8.RuneLen(airplane))
-	utf8.EncodeRune(byteAirplane, airplane)
+const (
+	offset    = 10
+	inputRune = '\u2708'
+)
 
+var expectedOutputRune = transformFunc(inputRune)
+var inputRuneBytes = getInputRuneBytes()
+
+var transformFunc = func(r rune) rune {
+	return algorithms.OffsetRuneForward(r, offset)
+}
+
+func getInputRuneBytes() []byte {
+	inputRuneBytes := make([]byte, utf8.RuneLen(inputRune))
+	utf8.EncodeRune(inputRuneBytes, inputRune)
+	return inputRuneBytes
+}
+
+func Test_transformRuneBuffers_allRunesCorrectlyRead(t *testing.T) {
+	// given
 	inputBuffer := new(bytes.Buffer)
-	inputBuffer.WriteRune(airplane)
-	inputBuffer.Write(byteAirplane[:2])
+	inputBuffer.WriteRune(inputRune)
+	inputBuffer.WriteRune(inputRune)
 
 	outputBuffer := new(bytes.Buffer)
 
-	transformFunc := func(r rune) rune {
-		return algorithms.OffsetRuneForward(r, 10)
-	}
+	expectedInputBuffer := getEmptyInitialisedBuffer()
+
+	expectedOutputBuffer := new(bytes.Buffer)
+	expectedOutputBuffer.WriteRune(expectedOutputRune)
+	expectedOutputBuffer.WriteRune(expectedOutputRune)
+
 	// when
 	err := transformRuneBuffers(inputBuffer, outputBuffer, transformFunc)
+	// then
+	assert.NoError(t, err)
+	assert.Equal(t, expectedInputBuffer, inputBuffer)
+	assert.Equal(t, expectedOutputBuffer, outputBuffer)
+}
+
+func Test_transformRuneBuffers_partialTransformationLastRuneNotComplete(t *testing.T) {
+	// given
+	inputBuffer := new(bytes.Buffer)
+	inputBuffer.WriteRune(inputRune)
+
+	inputRuneBytesSlice := inputRuneBytes[:2]
+	inputBuffer.Write(inputRuneBytesSlice)
+
+	outputBuffer := new(bytes.Buffer)
+
+	expectedErr := &ErrErroneousRune{}
+
+	expectedInputBuffer := new(bytes.Buffer)
+	expectedInputBuffer.Write(inputRuneBytesSlice)
+
+	expectedOutputBuffer := new(bytes.Buffer)
+	expectedOutputBuffer.WriteRune(expectedOutputRune)
+	// when
+	err := transformRuneBuffers(inputBuffer, outputBuffer, transformFunc)
+	// then
+	assert.Equal(t, expectedErr, err)
+	assert.Equal(t, expectedInputBuffer, inputBuffer)
+	assert.Equal(t, expectedOutputBuffer, outputBuffer)
+}
+
+func Test_transformRuneBuffers_partialTransformationLastRuneNotCompleteAddedMissingRunePart(t *testing.T) {
+	// given
+	inputBuffer := new(bytes.Buffer)
+	inputBuffer.WriteRune(inputRune)
+
+	inputRuneBytesSlice := inputRuneBytes[:2]
+	inputBuffer.Write(inputRuneBytesSlice)
+
+	outputBuffer := new(bytes.Buffer)
+
+	expectedErr := &ErrErroneousRune{}
+
+	expectedInputBuffer := getEmptyInitialisedBuffer()
+
+	expectedOutputBuffer := new(bytes.Buffer)
+	expectedOutputBuffer.WriteRune(expectedOutputRune)
+	expectedOutputBuffer.WriteRune(expectedOutputRune)
+	// when & then
+	err := transformRuneBuffers(inputBuffer, outputBuffer, transformFunc)
+	assert.Equal(t, expectedErr, err)
 	inputBuffer.WriteByte(136)
 	err = transformRuneBuffers(inputBuffer, outputBuffer, transformFunc)
-	fmt.Println(err)
+	assert.Equal(t, expectedInputBuffer, inputBuffer)
+	assert.Equal(t, expectedOutputBuffer, outputBuffer)
+}
+
+func Test_transformRuneBuffers_initialRuneIncomplete(t *testing.T) {
+	// given
+	inputBuffer := new(bytes.Buffer)
+	inputRuneBytesSlice := inputRuneBytes[:2]
+	inputBuffer.Write(inputRuneBytesSlice)
+
+	outputBuffer := new(bytes.Buffer)
+
+	expectedErr := &ErrErroneousInitialRune{}
+
+	expectedInputBuffer := new(bytes.Buffer)
+	expectedInputBuffer.Write(inputRuneBytesSlice)
+
+	expectedOutputBuffer := new(bytes.Buffer)
+	// when
+	err := transformRuneBuffers(inputBuffer, outputBuffer, transformFunc)
+	// then
+	assert.Equal(t, expectedErr, err)
+	assert.Equal(t, expectedInputBuffer, inputBuffer)
+	assert.Equal(t, expectedOutputBuffer, outputBuffer)
+}
+
+func getEmptyInitialisedBuffer() *bytes.Buffer {
+	buffer := new(bytes.Buffer)
+	buffer.Grow(64)
+	return buffer
 }
